7068c03523d7a33722e178dfe88a6ce4094ce323
Polly Yankova <89391410+PollyMG@users.noreply.github.com>
2022-07-08 13:43:14 +0300

    Add files via upload

---
+++ accordion.js
@@ -0,0 +1,62 @@
+async function solution() {
+    //fetch all articles from server
+    // create divs with titles / toggle buttons
+    // on click fetch details data
+    // update html with data
+    try {
+        let url = 'http://localhost:3030/jsonstore/advanced/articles/list';
+        let response = await fetch(url);
+
+        if(!response.ok){
+            throw new Error('Error obtaining article list') // descriptive errors
+        }
+
+        let data = await response.json()
+
+        data.forEach(articleInfo => {
+            let articleElement = document.createElement('div')
+            articleElement.classList.add('accordion')
+            articleElement.innerHTML = 
+            `
+            <div class="head">
+                <span>${articleInfo.title}</span>
+                <button class="button" id="${articleInfo._id}" onclick="moreOnclick(event)">More</button>
+            </div>
+            <div class="extra"></div>
+            `
+            let main = document.getElementById('main')
+            main.appendChild(articleElement)
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
+async function moreOnclick(e) {
+    try {
+        let currentTarget = e.currentTarget
+        let url = 'http://localhost:3030/jsonstore/advanced/articles/details/' + currentTarget.id
+        let parent = currentTarget.parentNode.parentNode;
+        let extraDiv = parent.querySelector('div.extra')
+
+        let response = await fetch(url)
+
+        if(!response.ok){
+            throw new Error('Error obtaining article details')
+        }
+        let data = await response.json()
+
+        extraDiv.innerHTML = `<p>${data.content}</p>`
+
+        if(currentTarget.textContent == 'More'){
+            currentTarget.textContent = 'Less'
+            extraDiv.style.display = 'block'
+        } else {
+            currentTarget.textContent = 'More'
+            extraDiv.style.display = 'none'
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+solution();
